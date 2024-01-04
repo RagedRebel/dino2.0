ca17a894a8699c25bad1b22398e383bf1640a0a8
RagedRebel <reubenav1305@gmail.com>
2024-01-04 23:31:50 +0530

    first commit

--- gametest.py
+++ gametest.py
@@ -4,6 +4,19 @@ pygame.init()
 screen_width=800
 screen_height=600
 
+#title and icon
+pygame.display.set_caption("Dino Game")
+icon=pygame.image.load("velociraptor.png")
+pygame.display.set_icon(icon)
+
+#player image
+playerimg=pygame.image.load("shark.png")
+px=120
+py=270
+def shark(x,y):
+	screen.blit(playerimg,(x,y))
+
+
 #screen creation
 screen=pygame.display.set_mode((screen_width,screen_height))
 
@@ -12,9 +25,10 @@ player=pygame.Rect((300,250,50,50))
 
 #game loop
 while run:
-	screen.fill((0,0,0))
-
-	pygame.draw.rect(screen,(255,255,255),player)
+	screen.fill((53, 118, 230))    #background fill
+	pygame.draw.rect(screen,(200,200,110),player)
+	shark(px,py)
+	
 	key=pygame.key.get_pressed()
 	if key[pygame.K_a]==True:
 		player.move_ip(-1,0)
@@ -30,6 +44,8 @@ while run:
 	for event in pygame.event.get():
 		if event.type==pygame.QUIT:
 			run=False
-	pygame.display.update()
+
+
+	pygame.display.update()   #updating the changes to the screen
 
 pygame.quit()
